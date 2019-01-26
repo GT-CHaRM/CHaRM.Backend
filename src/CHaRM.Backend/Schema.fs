d88de71058e3f18fc332dbd679eea6a43bd25676
Nima Shoghi <nimashoghi@gmail.com>
2019-01-26 02:18:44 -0500

    Fixed some documentation problems.

--- src/CHaRM.Backend/Schema.fs
+++ src/CHaRM.Backend/Schema.fs
@@ -1,6 +1,38 @@
 module CHaRM.Backend.Schema
 
-(* This module contains the Schema definition. The Schema  *)
+(*
+    This module contains the GraphQL Schema definition.
+    The GraphQL Schema provides us with a way of defining all of our data types in a language-independent manner.
+    For example, a user type might be the following in Java:
+    ```
+        public class User {
+            private String name;
+            private int age;
+
+            public Bicycle(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            public String getName() { return name; }
+            public String setName(String name) { this.name = name; }
+
+            public String getAge() { return age; }
+            public String setAge(String age) { this.age = age; }
+        }
+    ```
+    but that same user might be typed as such in TypeScript:
+    ```
+        interface User {
+            name: string;
+            age: number;
+        }
+    ```
+    and in JavaScript, there's no static typing so the object is just plain JSON!
+
+    GraphQL gives us a langauge to define our data in 1 language and have it work predictably across all of the languages we use that data in.
+    The point of the Schema module is to convert our F# data types into GraphQL schema types.
+*)
 
 open System
 open GraphQL.FSharp
@@ -9,7 +41,7 @@ open GraphQL.FSharp.Builder
 open CHaRM.Backend.Model
 open CHaRM.Backend.Provider
 
-(* Automatically converting our model to be Schema *)
+(* Automatically converting our model to Schema *)
 let ItemType = Auto.Object<ItemType>
 let ItemSubmissionBatch = Auto.Object<ItemSubmissionBatch>
 let Submission = Auto.Object<Submission>
